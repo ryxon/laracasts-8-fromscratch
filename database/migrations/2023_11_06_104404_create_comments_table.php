797fbd00eea43f1e038d57f61ff2096c70b323d3
Ryan Hendriks <ryanhendriks@live.nl>
2023-11-07 10:31:50 +0100

    - comments migration

--- database/migrations/2023_11_06_104404_create_comments_table.php
+++ database/migrations/2023_11_06_104404_create_comments_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id');
+            $table->foreignId('user_id');
+            $table->text('body');
+            $table->boolean('approved')->default(false);
+            $table->timestamp('approved_at')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->softDeletes();
+            //what are softdeleted?
+            //softdeletes are when you delete a record, it is not actually deleted from the database, but it is marked as deleted
             $table->timestamps();
         });
     }
