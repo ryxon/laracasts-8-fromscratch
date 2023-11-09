cf89d5144989e520bfecaac0ed9aa83fda4d26b9
Ryan Hendriks <ryanhendriks@live.nl>
2023-11-09 16:15:30 +0100

    - added author text field so when author is manually clicked it is later editable in the form and can be added on by the search form inputs.

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -1,4 +1,4 @@
-<header class="max-w-xl mx-auto mt-20 text-center">
+<header class="max-w-xxl mx-auto mt-20 text-center">
     <h1 class="text-4xl">
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
@@ -31,7 +31,7 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
 
         <form method="GET" action="#">
-
+{{--        data provided by the component, file: app/View/Components/CategoryDropdown.php    --}}
         <x-category-dropdown></x-category-dropdown>
 
         <!-- Other Filters -->
@@ -58,10 +58,9 @@
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-
-                <input type="text" name="search" placeholder="Find something" class="bg-transparent placeholder-black font-semibold text-sm" value="{{ request('search') }}">
-                <input type="submit" value="submit"/>
-
+                <input type="text" name="author" value="{{ request('author') }}" placeholder="author" class="p-3">
+                <input type="text" name="search" placeholder="Search..." class="p-3" value="{{ request('search') }}">
+                <input type="submit" value="submit" class="p-3"/>-<a href="/" class="p-3">reset</a>
         </div>
         </form>
     </div>
