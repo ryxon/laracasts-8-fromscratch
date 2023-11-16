154b753d803d31bccef2eeb43a1da705f8c0e8cd
Ryan Hendriks <ryanhendriks@live.nl>
2023-11-16 13:57:36 +0100

    - commenting

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -19,13 +19,17 @@ class RegisterController extends Controller
             'name' => 'required|max:255|min:3',
             'username' => 'required|max:255|min:3',
             'email' => 'required|email|min:7|max:255',
-            'password' => 'required|min:7|max:255|min:7'
+            'password' => 'required|min:7|max:255|min:7' //automatically hashed
         ]);
 
         //store user
         User::create($attributes);
 
         //return to register.create and show message
-        return redirect('/register')->with('success', 'Your account has been created.');
+        return redirect('/register')
+            ->with('success', 'Your account has been created.'); // what does with do?
+        // It flashes the message to the session for one request.
+        // This is useful for quick messages like "Item has been deleted".
+        // how is it displayed in the view? By using the session helper function: session('success') in the view
     }
 }
