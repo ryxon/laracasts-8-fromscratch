b266a9c26bd1f3bc3b1b7ff213dcf9242fd09527
Ryan Hendriks <ryanhendriks@live.nl>
2023-11-20 15:25:33 +0100

    Added vite dynamic tag for dev/build files switching.

--- resources/views/components/home_layout.blade.php
+++ resources/views/components/home_layout.blade.php
@@ -1,12 +1,13 @@
 <!doctype html>
 
-<title>Laravel From Scratch Blog</title>
+<title>Laravel From Scratch 8</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 {{-- inlcude google jquery cdn--}}
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-{{-- include google jquery cdn--}}
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
 <body style="font-family: Open Sans, sans-serif">
 <section class="px-6 py-8">
