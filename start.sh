179d4ad0e7e92d54c539e78a1c914aa2aca5a178
Ryan Hendriks <ryanhendriks@live.nl>
2023-11-23 12:10:58 +0100

    aliases added to start script for easy access

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+alias art="php artisan"
+alias migrate="php artisan migrate"
+alias migrate:refresh="php artisan migrate:refresh"
+alias migrate:reset="php artisan migrate:reset"
+alias migrate:rollback="php artisan migrate:rollback"
+alias migrate:status="php artisan migrate:status"
+alias tinker="php artisan tinker"
+alias serve="php artisan serve"
+
 # Start Laravel's built-in server
 php artisan serve &
 
