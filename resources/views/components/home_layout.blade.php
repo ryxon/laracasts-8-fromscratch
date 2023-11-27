8280ec5d3777d253e17b92133accc21756a3cc9d
Ryan Hendriks <ryanhendriks@live.nl>
2023-11-27 16:43:49 +0100

    Admin dropdown menu created.

--- resources/views/components/home_layout.blade.php
+++ resources/views/components/home_layout.blade.php
@@ -6,6 +6,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 {{-- inlcude google jquery cdn--}}
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@2.x.x/dist/alpine.min.js" defer></script>
+
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -19,17 +21,28 @@
             </a>
         </div>
 
-        <div class="mt-8 md:mt-0">
-{{--            @if (auth()->check())--}}
-            @auth
-                <a href="/logout" class="text-xs font-bold uppercase">Log Out</a>
-            @else
-                <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
-            @endif
-            <a href="#footer" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
-                Subscribe for Updates
-            </a>
+        <div class="flex md:mt-0 mt-8 w w-3/4 justify-end items-center">
+            <div style="display: inline-block">Welcome, <b>Ryan Hendriks</b>!</div>
+
+            <x-admin.dropdown>
+                <x-slot name="trigger">
+                    <button class="text-xs font-bold uppercase bg-blue-500 text-white py-2 px-4 rounded focus:outline-none hover:bg-blue-700">
+                        Manage
+                    </button>
+                </x-slot>
+
+                <x-slot name="links">
+                    <x-admin.dropdown-link href="/admin/dashboard">Dashboard</x-admin.dropdown-link>
+                    <x-admin.dropdown-link href="/admin/posts">Posts</x-admin.dropdown-link>
+                    <x-admin.dropdown-link href="/admin/post/create">New Post</x-admin.dropdown-link>
+                    <x-admin.dropdown-link href="/admin/users">Users</x-admin.dropdown-link>
+                    <x-admin.dropdown-link href="/admin/categories">Categories</x-admin.dropdown-link>
+                    <x-admin.dropdown-link href="/admin/comments">Comments</x-admin.dropdown-link>
+                </x-slot>
+            </x-admin.dropdown>
+
+            <a href="/logout" class="text-xs font-bold uppercase ml-4">Log Out</a>
+            <a href="#footer" class="bg-blue-500 ml-4 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">Subscribe for Updates</a>
         </div>
     </nav>
 
